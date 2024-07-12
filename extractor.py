d1ef90c9370180fec196218c05a8a6fd52896d9f
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2024-07-12 12:37:21 +0200

    Use basic extarctor to get txt of raw data

--- extractor.py
+++ extractor.py
@@ -1,10 +1,12 @@
 import pyshark
+from os import listdir
 
 # Load the PCAP file
 pcap_file_path = './data/sessions/mtls/session_mtls_open_before_and_stay_open.pcap'
 capture = pyshark.FileCapture(pcap_file_path)
 
 # Function to extract relevant details from packets
+# this function don't work for now
 def extract_packet_info(packet):
     info = {}
     if 'IP' in packet:
@@ -28,6 +30,16 @@ def extract_packet_info(packet):
 # packet_details = [extract_packet_info(packet) for packet in capture]
 
 # Display the first few packet details for inspection
-for packet in capture:
-    print(packet)
-    print("-----------------------------------------------------------------\n----------------------------------------------------------------")git
+
+path = "./data/sessions"
+for dir in listdir(path):
+    for file in listdir(path+"/"+dir):
+        with open("./converted-data/sessions/"+dir+"/"+file[0:-5]+"_txt_converted.txt", "w") as f:
+            for packet in pyshark.FileCapture("./"+path+"/"+dir+"/"+file):
+                f.write(str(packet))
+                f.write("-"*17+"\n"+"-"*17)
+            f.close()
+
+# for packet in capture:
+#     print(packet)
+#     print("-----------------------------------------------------------------\n----------------------------------------------------------------")
