5315e002ef2147251c54d031fe379bde41c59545
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2024-08-28 17:38:07 +0200

    remove old version non used files

--- scripts/check_format.py
+++
@@ -1,9 +0,0 @@
-# Check the output format of converted pcap to txt file.
-## As for now it is not implemented it will send only True
-
-def check(filename):
-    return True
-
-if __name__  == "__main__":
-    file = input() # TODO change with argument feeding
-    print(check(file))
